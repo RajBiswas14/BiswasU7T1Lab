b6267b7f93347d7578bcca19016d3ecdfb0c7cda
1e10Laptop <sample@bths.edu>
2023-01-31 10:58:37 -0500

    Code!

---
+++ src/CatRunner.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+public class CatRunner {
+    public static void main(String[] args) {
+        ArrayList<Cat> cats = new ArrayList<Cat>();
+        Cat cat1 = new Cat("Jonathan");
+        Cat cat2 = new Cat("Raj");
+        Cat cat3 = new Cat("Bob");
+        cats.add(cat1);
+        cats.add(cat2);
+        cats.add(cat3);
+        System.out.println(cats);
+        Cat cat4 = new Cat("Toby");
+        Cat replaceCat = cats.set(2, cat4);
+        System.out.println(cats);
+        System.out.println(replaceCat);
+        cat4.setName(cat4.getName() + " Meow");
+        System.out.println(cat4);
+        System.out.println(cats);
+        for (int i = 0; i < cats.size(); i++){
+            String Upname = cats.get(i).getName().toUpperCase();
+            cats.get(i).setName(Upname);
+            cats.add(new Cat("SAM"));
+            System.out.print(cats);
+        }
+    }
+}
---
+++ src/UsedCarLot.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+public class UsedCarLot {
+    private ArrayList<Car> inventory;
+
+    public UsedCarLot() {
+        inventory = new ArrayList<Car>();
+    }
+
+    public ArrayList<Car> getInventory() {
+        return inventory;
+    }
+
+    public void addCar(Car catToAdd) {
+        inventory.add(catToAdd);
+    }
+    public boolean Swap(int idx1, int idx2) {
+    if(idx1 < inventory.size() && idx2 < inventory.size()) {
+        Car car1 = inventory.get(idx1);
+        Car car2 = inventory.set(idx2, car1);
+        inventory.set(idx1, car2);
+        return true;
+    } else {
+        return false;
+    }
+
+    }
+}
